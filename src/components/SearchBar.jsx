abfc209b6564d11b2bd5837a415adf039a517a02
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2026-06-16 12:59:55 +0200

    Update SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,14 @@
-export default function SearchBar() {
+export default function SearchBar({ searchInput, setSearchInput }) {
   return (
     <>
-      <input type="text" className="search-bar" placeholder="Search for products..." />
+      <input
+        type="text"
+        name="searchInput"
+        className="search-bar"
+        placeholder="Search for products..."
+        onChange={(e) => setSearchInput(e.target.value)}
+        value={searchInput}
+      />
     </>
   );
 }
