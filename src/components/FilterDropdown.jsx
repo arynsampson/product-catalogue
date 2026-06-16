4a46fde6f1f4494022731ee427eabd6d7f4d7e54
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2026-06-16 13:00:05 +0200

    Updates

--- src/components/FilterDropdown.jsx
+++ src/components/FilterDropdown.jsx
@@ -1,14 +1,14 @@
-export default function FilterDropdown() {
+export default function FilterDropdown({ brands }) {
   return (
     <>
       <select name="cars" id="cars" required>
-        <option value="" disabled selected>
+        <option defaultValue="" disabled selected>
           Category filter
         </option>
-        <option value="volvo">Volvo</option>
+        {/* <option value="volvo">Volvo</option>
         <option value="saab">Saab</option>
         <option value="mercedes">Mercedes</option>
-        <option value="audi">Audi</option>
+        <option value="audi">Audi</option> */}
       </select>
     </>
   );
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,21 @@
 import FilterDropdown from '../components/FilterDropdown';
 import Header from '../components/Header';
+import ProductGrid from '../components/products/ProductGrid';
 import SearchBar from '../components/SearchBar';
 
-export default function Dashboard() {
+export default function Dashboard({ products, searchInput, setSearchInput }) {
+  const brands = products?.map((product) => {
+    if (product.brand !== undefined) return product;
+  });
+
+  console.log(brands);
+
   return (
     <>
-      <SearchBar />
-      <FilterDropdown />
-      <FilterDropdown />
+      <SearchBar searchInput={searchInput} setSearchInput={setSearchInput} />
+      <FilterDropdown brands={brands} />
+      {/* <FilterDropdown /> */}
+      <ProductGrid products={products} />
     </>
   );
 }
