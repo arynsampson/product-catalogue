4bba79782ad7c14dbf95004e3ed6be82bdb77a45
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2026-06-18 09:54:58 +0200

    Styling Product cards

--- src/components/products/ProductGridItem.jsx
+++ src/components/products/ProductGridItem.jsx
@@ -5,12 +5,15 @@ export default function ProductGridItem({ product, isFavourite, toggleFavourite 
         <img src={product.thumbnail} alt={product.title} className="product-img" />
       </div>
       <div className="product-information">
+        <p className="product-category">{product.category}</p>
         <p className="product-title">{product.title}</p>
-        <p className="product-price">R{product.price}</p>
-        <p className="product-rating">Rating: {product.rating}</p>
+        <p className="product-rating">
+          <span className="number-of-reviews">({product.reviews.length})</span>
+        </p>
         <button className="" onClick={() => toggleFavourite(product)}>
           {isFavourite ? 'Unfavourite' : 'Favourite'}
         </button>
+        <p className="product-price">R{product.price}</p>
       </div>
     </div>
   );
