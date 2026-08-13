3cd43efd1a752b27ceaba655c636cc82f1e3b221
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2026-08-13 09:53:17 +0200

    Update playwright to run webServer for testing

--- tests/global.spec.js
+++ tests/global.spec.js
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
   // Go to the starting url before each test.
-  await page.goto('http://localhost:5173');
+  await page.goto('/');
 });
 
 test('all products are visible', async ({ page }) => {
